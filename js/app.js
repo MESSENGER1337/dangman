abf2b7dd67e7344d22885c26917a6d9e321aa17d
MESSENGER1337 <186360316+MESSENGER1337@users.noreply.github.com>
2025-01-11 14:30:15 -0600

    Remove music for now, polish keyboard

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
-const audio = new Audio('assets/music/Dangman in the Shadows (2).mp3');
-const toggleButton = document.getElementById('toggleButton');
-const volumeSlider = document.getElementById('volumeSlider');
 const categories = ["colors", "countries", "planets", "cities"];
 const catColors = ["purple", "orange", "maroon", "silver"];
 const hintText = document.getElementById('hintText');
+const letterBtn = document.getElementsByClassName('letter');
+let arr = Array.from(letterBtn);
+
+console.log(arr);
 
 const randomCategory = Math.floor(Math.random() * categories.length);
 category = categories[randomCategory];
@@ -13,22 +14,54 @@ if (category === "colors") {
 }
 
 const randomWord = Math.floor(Math.random() * currentCategory.length);
-
 console.log(currentCategory[randomWord]);
 
-audio.volume = 0.15;
-volumeSlider.value = 0.5;
 
-toggleButton.addEventListener('click', () => {
-    if (audio.paused) {
-        audio.play();
-        toggleButton.textContent = "Pause";
-    } else {
-        audio.pause();
-        toggleButton.textContent = "Play";
-    }
-});
 
-volumeSlider.addEventListener('input', () => {
-    audio.volume = volumeSlider.value;
-});
+
+// let string = "";
+
+// arr.forEach(button => {
+//     button.addEventListener('click', (e) =>{
+//         if(e.target.innerHTML == '='){
+//             string = eval(string);
+//             input.value = string;
+//         }
+
+//         else if(e.target.innerHTML == 'AC'){
+//             string = "";
+//             input.value = string;
+//         }
+//         else if(e.target.innerHTML == 'DEL'){
+//             string = string.substring(0, string.Length-1);
+//             input.value = string;
+//         }
+//         else{
+//             string += e.target.innerHTML;
+//             input.value = string;
+//         }
+//     })
+// })
+
+
+// MUSIC STUFF
+// const audio = new Audio('assets/music/Dangman in the Shadows (2).mp3');
+// const toggleButton = document.getElementById('toggleButton');
+// const volumeSlider = document.getElementById('volumeSlider');
+
+// audio.volume = 0.15;
+// volumeSlider.value = 0.5;
+
+// toggleButton.addEventListener('click', () => {
+//     if (audio.paused) {
+//         audio.play();
+//         toggleButton.textContent = "Pause";
+//     } else {
+//         audio.pause();
+//         toggleButton.textContent = "Play";
+//     }
+// });
+
+// volumeSlider.addEventListener('input', () => {
+//     audio.volume = volumeSlider.value;
+// });
