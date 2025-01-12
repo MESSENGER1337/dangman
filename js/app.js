51774561319bd4d0483c5b1534678f2d2fd826f6
MESSENGER1337 <186360316+MESSENGER1337@users.noreply.github.com>
2025-01-12 01:26:16 -0600

    Almost there

--- js/app.js
+++ js/app.js
@@ -7,7 +7,6 @@ const categories = {
     animals: ["DOG", "CAT", "ELEPHANT", "LION"]
 };
 
-
 // RANDOM CATEGORY_____________________________________________________
 let hintText = document.getElementById('hintText');
 const randomCategoryKey = Object.keys(categories)[Math.floor(Math.random() * Object.keys(categories).length)];
@@ -18,10 +17,7 @@ hintText.textContent = randomCategoryKey;
 const currentWord = currentCategory[Math.floor(Math.random() * currentCategory.length)];
 console.log("Random word:", currentWord);
 
-// COUNT LETTERS_______________________________________________________
-function countLetters(currentWord) {
-    return currentWord.length;
-}
+// GET LETTERS_______________________________________________________
 function scanForLetters(currentWord, lettersToScan) {
     let foundLetters = [];
     for (const letter of currentWord) {
@@ -32,65 +28,52 @@ function scanForLetters(currentWord, lettersToScan) {
     return foundLetters;
 }
 
-// RETURN LETTERS COUNTED_____________________________________________
-const letterCount = countLetters(currentWord);
-console.log("Number of letters:", letterCount);
-
 // SELECT LETTER_____________________________________________________
 const letterBtn = document.getElementsByClassName('letter');
 const keyboard = Array.from(letterBtn);
 let letterSelected = "";
 keyboard.forEach(letterBtn => {
     letterBtn.addEventListener('click', (e) => {
+        letterBtn.classList.toggle('clicked');
         letterSelected = e.target.innerHTML.slice(0, 1);
         console.log("Letter selected:", letterSelected);
     });
 });
 
-// SCAN FOR LETTER___________________________________________________
-const selectBtn = document.getElementById('selectBtn');
-let letterToSearch = "";
-selectBtn.addEventListener('click', (e) => {
-    let letterToSearch = letterSelected;
-    const foundLetter = scanForLetters(currentWord, letterToSearch);
-    console.log("Letter searched:", letterToSearch);
-    console.log("Letter found:", foundLetter);
-});
-
-
 // BLANK WORD_________________________________________________________
 let wordDisplay = document.getElementById('wordDisplay');
+
+let guessedLetters = new Set();
+
 function createBlankedWord(currentWord) {
-    return "_ ".repeat(currentWord.length);
+    return currentWord.split("").map((letter) => {
+        if (guessedLetters.has(letter)) {
+            return letter;
+        } else {
+            return "_";
+        }
+    }).join(" ");
 }
-const blankedWord = createBlankedWord(currentWord);
-console.log("Blanked word:", blankedWord);
-wordDisplay.textContent = blankedWord;
+
+function updateWordDisplay() {
+    const blankedWord = createBlankedWord(currentWord);
+    wordDisplay.textContent = blankedWord;
+}
+
+updateWordDisplay();
 
 
+// SCAN FOR LETTER___________________________________________________
+const selectBtn = document.getElementById('selectBtn');
+selectBtn.addEventListener('click', (e) => {
+    const letterToSearch = letterSelected;
+    const foundLetter = scanForLetters(currentWord, letterToSearch);
+    if (foundLetter) {
+        guessedLetters.add(letterToSearch);
+    }
+    console.log("Letter searched:", letterToSearch);
+    console.log("Letter found:", foundLetter);
 
 
-
-
-
-// MUSIC STUFF
-// const audio = new Audio('assets/music/Dangman in the Shadows (2).mp3');
-// const toggleButton = document.getElementById('toggleButton');
-// const volumeSlider = document.getElementById('volumeSlider');
-
-// audio.volume = 0.15;
-// volumeSlider.value = 0.5;
-
-// toggleButton.addEventListener('click', () => {
-//     if (audio.paused) {
-//         audio.play();
-//         toggleButton.textContent = "Pause";
-//     } else {
-//         audio.pause();
-//         toggleButton.textContent = "Play";
-//     }
-// });
-
-// volumeSlider.addEventListener('input', () => {
-//     audio.volume = volumeSlider.value;
-// });
+    updateWordDisplay();
+});
